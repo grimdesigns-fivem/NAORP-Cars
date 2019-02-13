1dbfbf9d1e7a19e8b2e70562e9afe343aafd8c6d
GrimDesigns <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-02-13 14:10:42 -0700

    Add files via upload

---
+++ Trooper Pack/Highway/data/vehicle_names.lua
@@ -0,0 +1,36 @@
+	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
+end
+
+Citizen.CreateThread(function()
+	-- cvpi w/ liberty
+	AddTextEntry('0x54830233', 'State Slicktop CVPI')
+	-- Unmarked Taurus
+	AddTextEntry('0x5275AB9E', 'Unmarked Taurus')
+	-- 2015 charger
+	AddTextEntry('0x519EF9F7', 'State 2010 Charger')
+	-- 2014 explorer
+	AddTextEntry('0x43605D7A', 'Statee 2014 FPIU')
+	-- 2014 slicktop explorer
+	AddTextEntry('0x61A019F9', 'State 2014 Slicktop FPIU')
+	-- ford expedition
+	AddTextEntry('0xD3487D4C', 'State 2016 Chevy Tahoe')
+	-- 2016 explorer
+	AddTextEntry('0x7C264F05', 'State FPI Taurus')
+	-- cvpi w/ vector
+	AddTextEntry('0x65E42281', 'State Chevy Tahoe')
+	-- 2009 charger
+	AddTextEntry('0x9EB8142C', 'State Chevy Tahoe Slicktop')
+	-- ford f350
+	AddTextEntry('0xFA81CBBE', 'State CVPI')
+	-- hwaycar10
+	AddTextEntry('0x7DB7A427', 'State 2015 Charger')
+	-- Silverado Slicktop
+	AddTextEntry('0x6BE80088', '2017 Chevy Silverado')
+	-- BIKE
+	AddTextEntry('0xEB16ACC4', 'Badass State Bike')
+	-- hwaycar12 --Charger2013
+	AddTextEntry('0xEB67FF8E', 'Chevy 2013 Police Charger')
+	-- hwaycar13 --Caprice
+	AddTextEntry('0xD9575B6D', 'Nicks Chevy Caprice')
+	-- FLATBED2
+	AddTextEntry('0x8FC50F21', 'Lighted Flatbeed by JWalker')
